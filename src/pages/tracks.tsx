fb3e7b090fa73c7a05279fef1429d5cac79d3c4f
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2023-10-22 17:08:55 +0100

    first commit

---
+++ src/pages/tracks.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { Layout } from "../components";
+import { gql } from "../__generated__/";
+import { useQuery } from "@apollo/client";
+import QueryResult from "../components/query-result";
+import TrackCard from "../containers/track-card";
+
+
+const TRACKS = gql(`
+  # Query goes here
+  query GetTracks {
+    tracksForHome {
+      id
+      title
+      thumbnail
+      length
+      modulesCount
+      author {
+        id
+        name
+        photo
+      }
+  }
+}
+`);
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const Tracks = () => {
+  const { loading, error, data } = useQuery(TRACKS);
+  if (loading) return "Loading...";
+  if (error) return `Error! ${error.message}`;
+  return (
+    <Layout grid>
+      <QueryResult error={error} loading={loading} data={data}>
+        {data?.tracksForHome?.map((track) => (
+          <TrackCard key={track.id} track={track} />
+        ))}
+    </QueryResult>
+    </Layout>
+  )
+};
+
+export default Tracks;
